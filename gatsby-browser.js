955aba7b3abb2a626bab81f7e92fcb39f29a932d
SPDUK <spdevuk@gmail.com>
2020-04-18 20:00:13 +0100

    feat: install postcss & normalize.css

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,4 @@
 import 'prismjs/themes/prism.css'
 import 'hiq/dist/hiq.min.css'
+
+import 'normalize.css'
--- gatsby-config.js
+++ gatsby-config.js
@@ -76,6 +76,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-postcss`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,

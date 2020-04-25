c5c706dc9f2425338ca7d490a579af8922b76a14
SPDUK <spdevuk@gmail.com>
2020-04-25 22:31:52 +0100

    fix: try using gatsby-plugin-offline (PWA)

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,7 +79,7 @@ module.exports = {
     `gatsby-plugin-postcss`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
     {
       resolve: 'gatsby-plugin-html-attributes',
       options: {

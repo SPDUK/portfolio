a198cfa71b5f955c9a87aafbb4ce4ba4011cd61c
SPDUK <spdevuk@gmail.com>
2020-04-25 19:19:22 +0100

    refactor: BEM

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,24 +5,20 @@ import { Link } from 'gatsby'
 export default function hero() {
   return (
     <section className="hero">
-      <div className="hero-inner">
-        <div className="hero-copy">
+      <div className="hero__inner">
+        <div className="hero__copy">
           <h1>Hi, I'm Steve</h1>
-          <p className="hero-paragraph">
+          <p className="hero__paragraph">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas
             fugiat temporibus explicabo blanditiis omnis, ipsam ipsum odit!
             Ducimus magni debitis maiores in.
           </p>
-          <div className="hero-cta">
-            <Link className="button button-primary" to="/projects">
-              Projects
-            </Link>
-            <Link className="button" to="/blog">
-              Blog Posts
-            </Link>
+          <div className="hero__cta">
+            <Link to="/projects">Projects</Link>
+            <Link to="/blog">Blog Posts</Link>
           </div>
         </div>
-        <div className="hero-figure anime-element">
+        <figure className="hero__figure anime-element">
           <svg
             className="placeholder"
             width="528"
@@ -31,38 +27,20 @@ export default function hero() {
           >
             <rect width="528" height="396" style={{ fill: 'transparent' }} />
           </svg>
+          <div className="hero__box hero__box--01" data-rotation="45deg"></div>
+          <div className="hero__box hero__box--02" data-rotation="-45deg"></div>
+          <div className="hero__box hero__box--03" data-rotation="0deg"></div>
           <div
-            className="hero-figure-box hero-figure-box-01"
-            data-rotation="45deg"
-          ></div>
-          <div
-            className="hero-figure-box hero-figure-box-02"
-            data-rotation="-45deg"
-          ></div>
-          <div
-            className="hero-figure-box hero-figure-box-03"
-            data-rotation="0deg"
-          ></div>
-          <div
-            className="hero-figure-box hero-figure-box-04"
+            className="hero__box hero__box--04"
             data-rotation="-135deg"
           ></div>
-          <div className="hero-figure-box hero-figure-box-05"></div>
-          <div className="hero-figure-box hero-figure-box-06"></div>
-          <div className="hero-figure-box hero-figure-box-07"></div>
-          <div
-            className="hero-figure-box hero-figure-box-08"
-            data-rotation="-22deg"
-          ></div>
-          <div
-            className="hero-figure-box hero-figure-box-09"
-            data-rotation="-52deg"
-          ></div>
-          <div
-            className="hero-figure-box hero-figure-box-10"
-            data-rotation="-50deg"
-          ></div>
-        </div>
+          <div className="hero__box hero__box--05"></div>
+          <div className="hero__box hero__box--06"></div>
+          <div className="hero__box hero__box--07"></div>
+          <div className="hero__box hero__box--08" data-rotation="-22deg"></div>
+          <div className="hero__box hero__box--09" data-rotation="-52deg"></div>
+          <div className="hero__box hero__box--10" data-rotation="-50deg"></div>
+        </figure>
       </div>
     </section>
   )

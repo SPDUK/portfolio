a130bd38761d347a296fb1dab617261339877b4f
SPDUK <spdevuk@gmail.com>
2020-05-02 17:58:17 +0100

    refactor: import svgs from a utility file

---
+++ src/utils/svgs.js
@@ -0,0 +1,55 @@
+// importing them like this allows webpack to load all the svgs without additional network requests, tedious but faster
+import javascript from '../../content/assets/javascript.svg'
+import vue from '../../content/assets/vue.svg'
+import react from '../../content/assets/react.svg'
+import redux from '../../content/assets/redux.svg'
+import graphql from '../../content/assets/graphql.svg'
+import gatsby from '../../content/assets/gatsby.svg'
+import css from '../../content/assets/css.svg'
+import sass from '../../content/assets/sass.svg'
+// backend
+import nodejs from '../../content/assets/nodejs.svg'
+import ruby from '../../content/assets/ruby.svg'
+import rails from '../../content/assets/rails.svg'
+import elixir from '../../content/assets/elixir.svg'
+import phoenix from '../../content/assets/phoenix.svg'
+import crystal from '../../content/assets/crystal.svg'
+import mongodb from '../../content/assets/mongodb.svg'
+import postgresql from '../../content/assets/postgresql.svg'
+
+// tools
+import heroku from '../../content/assets/heroku.svg'
+import docker from '../../content/assets/docker.svg'
+import webpack from '../../content/assets/webpack.svg'
+import aws from '../../content/assets/aws.svg'
+import vscode from '../../content/assets/vscode.svg'
+import digitalocean from '../../content/assets/digitalocean.svg'
+import git from '../../content/assets/git.svg'
+import ubuntu from '../../content/assets/ubuntu.svg'
+
+export {
+  javascript,
+  vue,
+  react,
+  redux,
+  graphql,
+  gatsby,
+  css,
+  sass,
+  nodejs,
+  ruby,
+  rails,
+  elixir,
+  phoenix,
+  crystal,
+  mongodb,
+  postgresql,
+  heroku,
+  docker,
+  webpack,
+  aws,
+  vscode,
+  digitalocean,
+  git,
+  ubuntu,
+}

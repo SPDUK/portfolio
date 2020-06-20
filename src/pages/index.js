5d4c514e2d1eff7c5292a7967b3b45df3cbc08d4
SPDUK <spdevuk@gmail.com>
2020-06-20 21:58:29 +0100

    fix: use correct SEO title for home layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
+      <SEO title="Home" />
       <Hero />
       <Technologies />
     </Layout>
